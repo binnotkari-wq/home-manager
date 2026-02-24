59042bb208746328397a852d40ba26065b37c4bd
Benoit <benoit@2a02-8429-3054-0701-22bb-160e-0de8-89ce.rev.sfr.net>
2026-02-24 20:02:05 +0100

    Auto-sync [2a02-8429-3054-0701-22bb-160e-0de8-89ce.rev.sfr.net] : 2026-02-24 20:02:05

--- modules/CLI_user-tools.nix
+++ modules/CLI_user-tools.nix
@@ -2,19 +2,6 @@
 
 {
   home.packages = with pkgs; [
-    duf         # analyse  espace disque
-    powertop    # gestion d'énèrgie https://commandmasters.com/commands/powertop-linux/
-    stow        # Gestionnaire de dotfiles (gestion automatisée de liens symboliques vers ~/.config/  -> permet de faire un repo github rationnel)
-    stress-ng   # outil de stress CPU : stress-ng --cpu 0 --cpu-method matrixprod -v
-    python313   # prend 45 Mo. Préférer à la version 315 qui prend 130 Mo
-    s-tui       # Interface graphique CLI pour monitorer fréquence/température. Prend 49 Mo, dont la majorité en commun avec python 313
-    htop        # gestionnaire de processus
-    btop        # gestionnaire de processus, plus graphique
-    zellij      # un autre desktop, prend 92 Mo, dont la moitié est commune aux dépendances de python313
-    mc          # gestionnaire de fichiers. Prend 45 Mo, mais la majeur parti est commune aux dépendances de python313
-    mplayer     # lecteur video, ne prend que 12 Mo
-    pyradio     # webradio, prend 39 Mo + MPV ou mplayer
-    mdcat       # afficheur de fichiers Markdown, prend 13 Mo
     # lynx        # navigateur web
     # fzf         # recherche de fichiers
     # vtm         # un desktop, prend 17 Mo
